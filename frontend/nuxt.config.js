e1a76f105b17806d583d06f82c9d197a42b4ac2d
Aleksandr Talalaev <neuotq@gmail.com>
2022-06-14 17:01:14 +0300

    chore: nuxt auth

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -37,16 +37,28 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
   ],
+
+  // Add auth to router globaly
+  router: {
+    middleware: ['auth']
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-
+  auth: {
+    strategies: {
+      laravelSanctum: {
+        provider: 'laravel/sanctum',
+        url: 'ztt.test'
+      },
+    }
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
